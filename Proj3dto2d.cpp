603084f0cbfb0e409364ca0ecfc3fd4f27ab8d72
Yash Trikannad <yash.trikannad@gmail.com>
2019-02-18 20:22:33 -0500

    Update after suggestions from Clark

--- Proj3dto2d.cpp
+++ Proj3dto2d.cpp
@@ -1,179 +1,87 @@
+//  Yash Trikannad
+//  (For SubT DARPA)
+
+// Function to Project 3D Map to 2D Projections
+// Inputs - Location, Width of Slice, Axis, Map3d
+// Output - 2D Map in the required Axis
+
+
 #include <iostream>
 #include <Eigen/Dense>
 #include <unsupported/Eigen/CXX11/Tensor>
 #include "Proj3dto2d.h"
+#include <cmath>
+#define WIDTH 3;
+#define DIRECTION Y;
+#define LOCATION 8;
 
-class projection: public slice_coords, maps2d{
+void projection::Proj3dto2d(const Eigen::TensorMap<Eigen::Tensor<int, 3>> &Map3d,
+        Eigen::Tensor<int, 2> &Out, ProjAxis axis,const int &location){
 
-private:
-    maps2d _maps2d;
-    std::string _plane;
-    int _location;
-    int _width;
-    int _dims[3] = {0,0,0};
-    slice_coords slice;
+    this->_location = location;
+    const Eigen::Tensor<int, 3>::Dimensions& dims = Map3d.dimensions();
 
-public:
-    projection(){}
+    int ceil_width = ceil((this->_width)/2.);
 
-    projection(const int dims[],const int width,const std::string &plane,const int z_location)
-            : _maps2d(dims[0], dims[1]),_plane(plane),_location(z_location),_width(width)
-    {
-        this->_dims[0] = dims[0];
-        this->_dims[1] = dims[1];
-        this->_dims[2] = dims[2];
+    if((this->_location+ceil_width)>_dims[0] || (this->_location-ceil_width)<0) {
+        throw std::runtime_error("The Projection Slice of 2d Maps is out of bounds");
     }
 
-    void maxInSlice(const Eigen::Tensor<int, 3> &slice, std::string s) {
-
-        const Eigen::Tensor<int, 3>::Dimensions &dims_slice = slice.dimensions();
-
-        if (this->_plane == "XY") {
-            if (s == "l") {
-
-                for (size_t i = 0; i < this->_dims[0]; i++) {
-                    for (size_t j = 0; j < this->_dims[1]; j++) {
-                        int max = 0;
-                        for (size_t k = 0; k < dims_slice[2]; k++) {
-                            if (slice(i, j, k) > max) {
-                                max = slice(i, j, k);
-                            }
-                        }
-                        _maps2d._projection2d_lower(i, j) = max;
-                    }
-                }
-                return;
-            } else if (s == "u") {
-                for (size_t i = 0; i < this->_dims[0]; i++) {
-                    for (size_t j = 0; j < this->_dims[1]; j++) {
-                        int max = 0;
-                        for (size_t k = 0; k < dims_slice[2]; k++) {
-                            int x = slice(i, j, k);
-                            if (slice(i, j, k) > max) {
-                                max = slice(i, j, k);
-                            }
-                        }
-                        _maps2d._projection2d_upper(i, j) = max;
-                    }
-                }
-                return;
-            }
-        }
-
-        if (this->_plane == "XZ") {
-            if (s == "l") {
-
-                for (size_t i = 0; i < this->_dims[2]; i++) {
-                    for (size_t j = 0; j < this->_dims[0]; j++) {
-                        int max = 0;
-                        for (size_t k = 0; k < dims_slice[1]; k++) {
-                            if (slice(j, k, i) > max) {
-                                max = slice(j, k, i);
-                            }
-                        }
-                        _maps2d._projection2d_lower(i, j) = max;
-                    }
-                }
-                return;
-            } else if (s == "u") {
-                for (size_t i = 0; i < this->_dims[2]; i++) {
-                    for (size_t j = 0; j < this->_dims[0]; j++) {
-                        int max = 0;
-                        for (size_t k = 0; k < dims_slice[1]; k++) {
-                            int x = slice(j, k, i);
-                            if (slice(j, k, i) > max) {
-                                max = slice(j, k, i);
-                            }
-                        }
-                        _maps2d._projection2d_upper(i, j) = max;
-                    }
-                }
-                return;
-            }
-        }
-
-        if (this->_plane == "YZ") {
-            if (s == "l") {
-
-                for (size_t i = 0; i < this->_dims[1]; i++) {
-                    for (size_t j = 0; j < this->_dims[2]; j++) {
-                        int max = 0;
-                        for (size_t k = 0; k < dims_slice[0]; k++) {
-                            if (slice(k, i, j) > max) {
-                                max = slice(k, i, j);
-                            }
-                        }
-                        _maps2d._projection2d_lower(j, i) = max;
-                    }
-                }
-                return;
-            } else if (s == "u") {
-                for (size_t i = 0; i < this->_dims[1]; i++) {
-                    for (size_t j = 0; j < this->_dims[2]; j++) {
-                        int max = 0;
-                        for (size_t k = 0; k < dims_slice[0]; k++) {
-                            if (slice(k, i, j) > max) {
-                                max = slice(k, i, j);
-                            }
-                        }
-                        _maps2d._projection2d_upper(j, i) = max;
-                    }
-                }
-                return;
-            }
-        }
+    // YZ Plane
+    if (axis == X){
+        slice.offsets = {this->_location - (this->_width)/2, 0, 0};
+        slice.extents = {(this->_width)/2 + ceil_width, this->_dims[1], this->_dims[2]};
     }
-
-    void Proj3dto2d(const Eigen::TensorMap<Eigen::Tensor<int, 3>> &Map3d,\
-     Eigen::Tensor<int, 2> &outLower,Eigen::Tensor<int, 2> &outUpper){
-
-        const Eigen::Tensor<int, 3>::Dimensions& dims = Map3d.dimensions();
-
-        int ceil_width = ceil((this->_width)/2.);
-
-        // Lower Slice does not contain the current location. Should I add it ?
-        // Upper Slice is the current location + width/2
-
-        // XY Plane
-        if (this->_plane == "XY") {
-            slice.offsets_lower = {0, 0, this->_location - (this->_width)/2};
-            slice.extents_lower = {this->_dims[0], this->_dims[1], (this->_width)/2};
-            slice.offsets_upper = {0, 0, this->_location};
-            slice.extents_upper = {this->_dims[0], this->_dims[1], ceil_width};
-        }
-
-        // YZ Plane
-        else if (this->_plane == "YZ"){
-            slice.offsets_lower = {this->_location - (this->_width)/2, 0, 0};
-            slice.extents_lower = {(this->_width)/2, this->_dims[1], this->_dims[2]};
-            slice.offsets_upper = {this->_location, 0, 0};
-            slice.extents_upper = {ceil_width, this->_dims[1], this->_dims[2]};
-        }
 
         // XZ Plane3
-        else if (this->_plane == "XZ"){
-            slice.offsets_lower = {0,  this->_location - (this->_width)/2, 0};
-            slice.extents_lower = {this->_dims[0], (this->_width)/2, this->_dims[2]};
-            slice.offsets_upper = {0, this->_location, 0};
-            slice.extents_upper = {this->_dims[0],  ceil_width, this->_dims[2]};
-        }
-
-        Eigen::Tensor<int, 3> lower_slice = Map3d.slice(slice.offsets_lower, slice.extents_lower);
-        Eigen::Tensor<int, 3> upper_slice = Map3d.slice(slice.offsets_upper, slice.extents_upper);
-
-        this->maxInSlice(lower_slice, "l");
-        this->maxInSlice(upper_slice, "u");
-
-        outLower = _maps2d._projection2d_lower;
-        outUpper = _maps2d._projection2d_upper;
-
+    else if (axis == Y){
+        slice.offsets = {0,  this->_location - (this->_width)/2, 0};
+        slice.extents = {this->_dims[0], (this->_width)/2 + ceil_width, this->_dims[2]};
+    }
+        // Z Axis
+    else if (axis == Z) {
+        slice.offsets = {0, 0, this->_location - (this->_width)/2};
+        slice.extents = {this->_dims[0], this->_dims[1], (this->_width)/2 + ceil_width};
+    }
+    else{
+        throw std::runtime_error("Axis is not Correct. Mention the Perpendicular Axis as X, Y or Z");
     }
 
-};
+    Eigen::Tensor<int, 3> lower_slice = Map3d.slice(slice.offsets, slice.extents);
+
+    this->maxInSlice(lower_slice, axis);
+
+    Out = _maps2d._projection2d;
+}
+
+void projection::maxInSlice(const Eigen::Tensor<int, 3> &slice, ProjAxis axis) {
+
+    if (axis == X){
+        Eigen::array<int, 1> dims({0});
+        _maps2d._projection2d = slice.maximum(dims);
+    }
+    else if (axis == Y){
+        Eigen::array<int, 1> dims({1});
+        _maps2d._projection2d = slice.maximum(dims);
+    }
+    else if(axis == Z){
+        Eigen::array<int, 1> dims({2});
+        _maps2d._projection2d = slice.maximum(dims);
+    }
+    else{
+        throw std::runtime_error("Axis is not Correct. Mention the Perpendicular Axis as X, Y or Z");
+    }
+
+    Eigen::array<int, 2> shuffling({1, 0});
+    _maps2d._projection2d = _maps2d._projection2d.shuffle(shuffling).eval();
+}
+
 
 int main() {
 
-// Populate the input voxel map
+//////////////////////////////////////////////////////////////////////////////////////
+////////////////               TEST MAP DEFINITION                   /////////////////
+//////////////////////////////////////////////////////////////////////////////////////
     Eigen::Tensor<int, 3> t_3d(10, 10, 10);
     t_3d = t_3d.constant(1);
 
@@ -189,13 +97,14 @@ int main() {
     Eigen::Tensor<int, 3> slice3 = t_3d.slice(offsets3, extents2).setConstant(2);
     Eigen::Tensor<int, 3> slice4 = t_3d.slice(offsets4, extents2).setConstant(2);
 
-    // 2D Map stored as TensorMap
+    // 3D Map pointed by TensorMap
     Eigen::TensorMap<Eigen::Tensor<int , 3>> map_3d(t_3d.data(), 10, 10, 10);
+///////////////////////////////////////////////////////////////////////////////////////
 
     // Input Parameters
-    int width = 5;
-    std::string plane = "XY";
-    int location = 5;
+    int ProjectWidth = WIDTH;
+    ProjAxis ProjectDir = DIRECTION;
+    int ProjLoc = LOCATION;
 
     const Eigen::Tensor<int, 3>::Dimensions& dims = map_3d.dimensions();
     int dimMap[3];
@@ -203,15 +112,13 @@ int main() {
     dimMap[1] = dims[1];
     dimMap[2] = dims[2];
 
-    projection p(dimMap, width, plane, location);
+    projection p(dimMap, ProjectWidth, ProjectDir, ProjLoc);
 
-    Eigen::Tensor<int, 2> projection2d_lower;
-    Eigen::Tensor<int, 2> projection2d_upper;
+    Eigen::Tensor<int, 2> ProjectionMap2d;
 
-    p.Proj3dto2d(map_3d, projection2d_lower, projection2d_upper);
+    p.Proj3dto2d(map_3d, ProjectionMap2d, ProjectDir, ProjLoc);
 
-    std::cout << projection2d_lower << std::endl;
-    std::cout << projection2d_upper <<std::endl;
+    std::cout << ProjectionMap2d << std::endl;
 
     return 0;
     }
--- Proj3dto2d.h
+++ Proj3dto2d.h
@@ -1,31 +1,58 @@
-//
-// Created by yash on 2/16/19.
-//
+//  Header File for Projection from 3D to 2D
+//  Yash Trikannad
+//  (For SubT DARPA)
 
 #pragma once
-
-#include <iostream>
-#include <Eigen/Dense>
 #include <unsupported/Eigen/CXX11/Tensor>
 
 struct slice_coords{
-
-    Eigen::array<int, 3> offsets_lower;
-    Eigen::array<int, 3> extents_lower;
-    Eigen::array<int, 3> offsets_upper;
-    Eigen::array<int, 3> extents_upper;
-
+    Eigen::array<int, 3> offsets;
+    Eigen::array<int, 3> extents;
 };
 
 struct maps2d{
-
-    Eigen::Tensor<int, 2> _projection2d_lower;
-    Eigen::Tensor<int, 2> _projection2d_upper;
-
+    Eigen::Tensor<int, 2> _projection2d;
     maps2d(){}
-
     maps2d(int dim1, int dim2)
-    :  _projection2d_lower(Eigen::Tensor<int ,2>(dim1, dim2)), _projection2d_upper(Eigen::Tensor<int ,2>(dim1, dim2))
+            :  _projection2d(Eigen::Tensor<int ,2>(dim1, dim2))
     {}
 };
+
+enum ProjAxis{X, Y, Z};
+
+class projection: public slice_coords, maps2d{
+
+private:
+    maps2d _maps2d;
+    int _dims[3] = {0,0,0};
+    slice_coords slice;
+
+public:
+    int _location;
+    int _width;
+    ProjAxis _axis;
+
+    projection(){}
+
+    projection(const int dims[],const int width,ProjAxis axis,const int location)
+            : _maps2d(dims[0], dims[1]),_width(width), _axis(axis),_location(location)
+    {
+        this->_dims[0] = dims[0];
+        this->_dims[1] = dims[1];
+        this->_dims[2] = dims[2];
+    }
+
+    void maxInSlice(const Eigen::Tensor<int, 3> &ProjectSlice, ProjAxis axis);
+
+    void Proj3dto2d(const Eigen::TensorMap<Eigen::Tensor<int, 3>> &Map3d,\
+     Eigen::Tensor<int, 2> &Out, ProjAxis axis, const int &location);
+
+};
+
+
+
+
+
+
+
 
